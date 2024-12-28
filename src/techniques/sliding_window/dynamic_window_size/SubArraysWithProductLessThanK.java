5c323017450864d7e78756a428212dbd75f23704
harshavardhanyadlapalli <harsha00032@gmail.com>
2024-12-28 18:53:12 +0530

    add SubArraysWithProductLessThanK.java

---
+++ src/techniques/sliding_window/dynamic_window_size/SubArraysWithProductLessThanK.java
@@ -0,0 +1,41 @@
+package techniques.sliding_window.dynamic_window_size;
+
+/*
+Given an array with positive numbers and a positive target number, find all of its contiguous subarrays whose product is less than the target number.
+Example 1:
+Input: [2, 5, 3, 10], target=30
+Output: [2], [5], [2, 5], [3], [5, 3], [10]
+Explanation: There are six contiguous subarrays whose product is less than the target.
+ */
+public class SubArraysWithProductLessThanK {
+
+    private static int subArraysWithProductLessThanK(int[] nums, int target)  {
+
+        int start = 0;
+        int end = 0;
+        int product = 1;
+        int count = 0;
+
+        while (end < nums.length) {
+            product *= nums[end];
+
+            //shrink the window till your product is less than the target
+            while (product >=target && start <= end){
+                product /= nums[start];
+                start++;
+            }
+
+            count += end-start+1;
+            end++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 3, 10};
+        System.out.println("The number of sub arrays with count less than target => "+ subArraysWithProductLessThanK(nums, 30));
+
+    }
+
+}
